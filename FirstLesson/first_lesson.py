0d4c411d752da6886d3887aaf5b570a7e80e8456
Femin Justin <feminjustin07@gmail.com>
2025-07-09 23:30:26 +0530

    python first lesson about print, input and variables

---
+++ FirstLesson/first_lesson.py
@@ -0,0 +1,5 @@
+# to add a variable to the code, you can use the following syntax:
+# variable_name = input("What is your variable? ")
+user_name = input("What is your name? ")
+print("Hello, " + user_name + "! Welcome to the program.")
+# This code prompts the user for their name and then greets them.
---
+++ FirstLesson/name_generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Name Generator!")
+print("This program will help you create a new name by adding a middle name to your existing name.")
+
+first_name=input("What is your first name? ")
+last_name=input("What is your last name? ")
+middle_name=input("What is the middle name you want? ")
+
+print("Your brand new name is: " + first_name + " " + middle_name + " " + last_name)
