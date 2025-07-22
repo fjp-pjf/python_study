40cae56171389078a25609cbaad19bfb056cb5b0
fjp-pjf <feminjustin07@gmail.com>
2025-07-22 23:56:47 +0530

    Password generator

---
+++ FifthLesson/PasswordChecker.py
@@ -0,0 +1,18 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Toolie Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+random_required_letters = random.sample(letters, nr_letters)
+random_required_numbers = random.sample(numbers, nr_numbers)
+random_required_symbols = random.sample(symbols, nr_symbols)
+
+password_tobe_generated = random.shuffle(random_required_letters + random_required_numbers + random_required_symbols)
+
+generated_password = "".join(password_tobe_generated)
