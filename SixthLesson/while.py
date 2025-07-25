2b20ac2a26fbe6efbfb4789cfe7631eb14bf4053
fjp-pjf <feminjustin07@gmail.com>
2025-07-25 23:51:11 +0530

    loops

---
+++ SixthLesson/while.py
@@ -0,0 +1,14 @@
+# while loops and for loops notes
+
+# Print numbers from 1 to 5 using while loop
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+# Print each item in a list using for loop
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# while loop is same as js while loop but for loop in python is same as for-in loop in js
