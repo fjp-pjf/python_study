e3e411669f7d550c7d4c540e7a97878982131a63
fjp-pjf <feminjustin07@gmail.com>
2025-08-21 09:50:05 +0530

    weeks left code added

---
+++ EighthLesson/time_left.py
@@ -0,0 +1,9 @@
+def life_in_weeks(age):
+    years_left = 75 - age
+    
+    total_weeks_left = (years_left * 52)
+    
+    print(f"You have {total_weeks_left} weeks left.")
+   
+life_in_weeks(28)
+    
