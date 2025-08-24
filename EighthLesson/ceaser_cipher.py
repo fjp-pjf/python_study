c142e78c87c598e068a07b628b00cbd848322f9a
fjp-pjf <feminjustin07@gmail.com>
2025-08-24 21:53:41 +0530

    caesar cipher completed

--- EighthLesson/ceaser_cipher.py
+++ EighthLesson/ceaser_cipher.py
@@ -4,27 +4,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-def encrypt(original_text, shift_amount):
-    encoded_word = ""
+def caesar(shift_direction, original_text, shift_amount):
+    cipher_text = ""
     for letter in original_text:
-        og_letter_idx = alphabet.index(letter)
-
-        shifted_position = og_letter_idx + shift_amount
+        if shift_direction == 'encode':
+            shifted_position = alphabet.index(letter) + shift_amount
+        else:
+            shifted_position = alphabet.index(letter) - shift_amount
         shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
 
-        shifted_letter = alphabet[shifted_position]
-        encoded_word += shifted_letter
+    print(f"Here is the original text: {cipher_text}")
 
-    print(encoded_word)
+caesar(direction, text, shift)
 
-encrypt(text, shift)
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
 
