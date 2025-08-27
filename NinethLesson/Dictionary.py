6f1a2bf467accfc017b81a9450eaedb808271566
fjp-pjf <feminjustin07@gmail.com>
2025-08-27 21:38:25 +0530

    dictionary example

--- NinethLesson/Dictionary.py
+++ NinethLesson/Dictionary.py
@@ -17,3 +17,26 @@ print(family_jargon)
 family_jargon["I_want"] = "to be Married and be happy with 3 kids"
 print(family_jargon)
 
+# example program
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+
+student_grades = {}
+
+for student in student_scores:
+    if student_scores[student] >= 91 and student_scores[student] <= 100:
+        student_grades[student] = "Outstanding"
+    elif student_scores[student] >= 81 and student_scores[student] <= 90:
+        student_grades[student] = "Exceeds Expectations"
+    elif student_scores[student] >= 71 and student_scores[student] <= 80:
+        student_grades[student] = "Acceptable"
+    elif student_scores[student] <= 70:
+        student_grades[student] = "Fail"
+        
+print(student_grades)
