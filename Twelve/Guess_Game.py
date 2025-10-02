2f5c56e533d7c75ec78566d631ac867e8bc251aa
fjp-pjf <feminjustin07@gmail.com>
2025-10-02 12:45:02 +0530

    guessing game completed

--- Twelve/Guess_Game.py
+++ Twelve/Guess_Game.py
@@ -1,21 +1,34 @@
-# we have to do a number guessing game
-# two levels -> easy, medium
-# easy 10, hard 5 chances, the computer thinks of a random number
-# when the user submits their guess, we have to say too high, too low
-# when user guess correctly, show them congrats
 import random
 
 num_list = [12, 19, 23, 43, 82, 99, 2, 77]
 
+def check_guess(user_guess, computer_choice):
+    if user_guess == computer_choice:
+        print("Congrats! You guessed the correct number")
+        return True
+    elif user_guess < computer_choice:
+        print("Ooh! Too low.")
+    else: 
+        print("Oh! Too high.")
+    return False
+
 def guessing_game():
     print("Let's play a number guessing game")
-    game_level = input("Choose your difficulty: easy | hard")
+    game_level = input("Choose your difficulty: easy | hard ").lower()
     computer_choice = random.choice(num_list)
 
     if game_level == "easy":
-        for index in range(1, 10):
-            print(f"You have 10 guesses. what is your {index} guess?")
-            
+        for index in range(1, 11):
+            guess = int(input(f"You have {11 - index} guesses. What is your {index} guess? "))
+            if check_guess(guess, computer_choice):
+                return
+
     if game_level == "hard":
-        for index in range(1, 5):
-            print(f"You have 5 guesses. what is your {index} guess?")
+        for index in range(1, 6):
+            guess = int(input(f"You have {6 - index} guesses. What is your {index} guess? "))
+            if check_guess(guess, computer_choice):
+                return
+
+    print(f"Sorry, you're out of guesses. The number was {computer_choice}.")
+
+guessing_game()
